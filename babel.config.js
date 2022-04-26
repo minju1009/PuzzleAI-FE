dcf03e37d181e2252dbf67ecea83a8849d79b0e8
Minju <minju1009@gmail.com>
2022-04-26 15:32:22 +0900

    Modify : tsconfig 절대경로 사용하도록 수정

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,30 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['babel-plugin-styled-components'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['babel-plugin-styled-components'],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import {ThemeProvider} from 'styled-components';
-import theme from './src/styles/theme';
+import theme from 'styles/theme';
 import Navigator from './Navigator';
 
 const ProvidedNavigator = () => {
